234cea8754b6567ada21fa9242cd0e65482ffd72
Samantha Hua <shua30@stuy.edu>
2021-12-10 13:16:07 -0500

    classwork

---
+++ ALTester.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ALTester {
+  int[] array = ArrayList(23);
+
+  public ALTester {
+    for (int i=0; i<array.length; i++) {
+      int[i] = (int) 10*Math.random();
+    }
+  }
+
+  public boolean sortedOrNot(int[] a) {
+    for
+  }
+
+
+
+}
