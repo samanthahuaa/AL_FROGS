967ff674c85be8a44ca918e98a27c64682587fd3
samanthahuaa <samanthahua1@gmail.com>
2021-12-11 20:56:37 -0500

    homework

--- ALTester.java
+++ ALTester.java
@@ -1,18 +1,36 @@
 import java.util.ArrayList;
 
 public class ALTester {
-  int[] array = ArrayList(23);
-
-  public ALTester {
-    for (int i=0; i<array.length; i++) {
-      int[i] = (int) 10*Math.random();
+  public static int compareTo(int a, int b) {
+		if (a > b) {
+			return -1;
+		} else {
+      return 1;
     }
+	}
+
+  public static boolean sortedOrNot(ArrayList<Integer> a) {
+    for (int i=0; i<a.size()-1; i++) {
+      if (compareTo(a.get(i),a.get(i+1)) == -1) {
+        return false;
+      }
+    }
+    return true;
   }
 
-  public boolean sortedOrNot(int[] a) {
-    for
+  public static void main(String[] args) {
+    ALTester boop = new ALTester();
+    ArrayList<Integer> hewwo = new ArrayList<Integer>();
+    for (int i=0; i<23; i++) {
+      hewwo.add((int) (10*Math.random()));
+    }
+    System.out.println(sortedOrNot(hewwo));
+
+    ArrayList<Integer> increasing = new ArrayList<Integer>();
+    for (int i=0; i<23; i++) {
+      increasing.add(i);
+    }
+    System.out.println(sortedOrNot(increasing));
   }
-
-
 
 }
